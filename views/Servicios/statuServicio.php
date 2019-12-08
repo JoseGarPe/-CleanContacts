a8850c412f7b4c7db114f5a73b0ba2a26d9b9740
JoseGarPe <jhosuegarciastarkand@gmail.com>
2019-12-07 21:44:19 -0600

    0712

--- views/Servicios/statuServicio.php
+++ views/Servicios/statuServicio.php
@@ -1,7 +1,7 @@
-<form role="form" action="../controllers/UsuarioController.php?accion=status" method="POST">
+<form role="form" action="../controllers/ServicioController.php?accion=status" method="POST">
               <div class="box-body">
 <?php 
-require_once "../../class/Usuario.php";
+require_once "../../class/Servicios.php";
                $codigo=$_POST["employee_id"];
                $estado=$_POST["employee_status"];
                if ($estado=='Inactivo') {
@@ -9,20 +9,20 @@ require_once "../../class/Usuario.php";
                }else{
                 $cambio='Activar';
                }
-					     $actividads = new Usuario();
+					     $actividads = new Servicios();
                          $dato = $actividads->selectOne($codigo);
                         
                       foreach ((array)$dato as $row) {
                          		echo '
 
-                            <label>¿Desea '.$cambio.' el usuario: <strong> '.$row['nombre'].' '.$row['apellido'].'</strong>?</label>
-                          <input type="hidden" name="id" id="id" value="'.$row['id_usuario'].'"/> 
+                            <label>¿Desea '.$cambio.' el Servicio: <strong> '.$row['nombre'].' </strong>?</label>
+                          <input type="hidden" name="id" id="id" value="'.$row['id_servicio'].'"/> 
                            <input type="hidden" name="estado" id="estado" value="'.$estado.'"/>  
                              ';}
 ?>
       </div>
               <div class="box-footer">
                 <input type="submit" class="btn btn-primary" name="submit" value="Confirmar" >
-                <input type="button" class="btn btn-danger" onClick="location.href = '../list/Usuarios.php'" name="cancel" value="Cancelar" >
+                <input type="button" class="btn btn-danger" onClick="location.href = '../list/Servicios.php'" name="cancel" value="Cancelar" >
               </div>
             </form>
